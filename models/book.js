b81a8518c355c51bd9f2fc817d874cbb9f66d815
Cole <cole.smith.code@gmail.com>
2022-10-18 17:32:14 -0600

    a lot

---
+++ models/book.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Book extends Model {}
+  Book.init({
+    title: { 
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please provide a value for "title"',
+        },
+      },
+    },
+    author: {
+     type: DataTypes.STRING,
+     allowNull: false,
+     validate: {
+        notNull: {
+          msg: 'Please provide a value for "author"'
+        },
+      },
+    },
+
+    genre: DataTypes.STRING,
+    year: DataTypes.INTEGER
+    
+  }, {
+    sequelize,
+    modelName: 'Book',
+  });
+  return Book;
+};
