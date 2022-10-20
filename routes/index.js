e3c60629f1e01f59285fbb2c0e68e4b2ad6d7fc6
Cole <cole.smith.code@gmail.com>
2022-10-19 22:00:57 -0600

    book route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const Book = require('../models').Book;
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', asyncHandler(async (req, res) => {
+  res.redirect('books');
+}));
 
 module.exports = router;
