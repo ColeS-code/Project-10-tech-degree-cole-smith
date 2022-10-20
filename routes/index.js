7aadbc99c975990d6a9d201225f625c543b748eb
Cole <cole.smith.code@gmail.com>
2022-10-20 16:46:00 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,17 @@ var express = require('express');
 var router = express.Router();
 const Book = require('../models').Book;
 
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
+
 /* GET home page. */
 router.get('/', asyncHandler(async (req, res) => {
   res.redirect('books');
